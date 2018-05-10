f1b51ff69ba0e084709275a1b6bd384075b30942
lvjunxiao <594076608@qq.com>
2018-05-10 15:45:17 +0800

    13

--- Aquaivy.Unity/Tasks/DelayTask.cs
+++ Aquaivy.Unity/Tasks/DelayTask.cs
@@ -19,7 +19,97 @@ namespace Aquaivy.Unity.Tasks
         Time
     }
 
-    class DelayTask
+    /// <summary>
+    /// 延迟任务，可按照帧数或者ms毫秒值来延迟
+    /// </summary>
+    public class DelayTask
     {
+        /// <summary>
+        /// 延迟类型
+        /// </summary>
+        DelayType Type;
+
+        /// <summary>
+        /// 累计时间
+        /// </summary>
+        public int Timer { get { return taskLite.Timer; } }
+        public int TimerRemaining { get { return Delay - taskLite.Timer; } }
+
+        /// <summary>
+        /// 当前执行过的帧数
+        /// </summary>
+        public int Frame { get { return taskLite.Frame; } }
+        public int FrameRemaining { get { return Delay - taskLite.Frame; } }
+
+        /// <summary>
+        /// 需要延迟的数量
+        /// </summary>
+        public int Delay;
+
+        /// <summary>
+        /// 关联数据
+        /// </summary>
+        public object Tag;
+
+        public Action Task;
+
+        private TaskLite taskLite;
+
+        public void Release()
+        {
+            Release(this);
+        }
+
+        /// <summary>
+        /// 压入一个延迟函数，可按照时间或者帧数来延迟
+        /// </summary>
+        /// <param name="task"></param>
+        /// <param name="delay"></param>
+        /// <param name="type"></param>
+        /// <param name="tag"></param>
+        /// <returns></returns>
+        public static DelayTask Invoke(Action task, int delay, DelayType type = DelayType.Time, object tag = null)
+        {
+            var delayTask = new DelayTask
+            {
+                Task = task,
+                Delay = delay,
+                Type = type,
+                Tag = tag
+            };
+
+            delayTask.taskLite = TaskLite.Invoke(t =>
+            {
+                if (type == DelayType.Time)
+                {
+                    if (t < delay)
+                        return false;
+                }
+                else if (type == DelayType.Frame)
+                {
+                    if (delayTask.Frame < delay)
+                        return false;
+                }
+
+                task();
+
+                return true;
+            });
+
+            return delayTask;
+        }
+
+        public static void Release(DelayTask delayTask)
+        {
+            delayTask.taskLite.Release();
+        }
+
+        public override string ToString()
+        {
+            if (Type == DelayType.Time)
+                return $"DelayTask tag={Tag}  type={Type}  Time={Timer}/{Delay}";
+            else
+                return $"DelayTask tag={Tag}  type={Type}  Frame={Frame}/{Delay}";
+        }
     }
 }
--- Aquaivy.Unity/Tasks/TaskLite.cs
+++ Aquaivy.Unity/Tasks/TaskLite.cs
@@ -18,7 +18,7 @@ namespace Aquaivy.Unity.Tasks
         public int Timer;
 
         /// <summary>
-        /// 当前执行过的帧
+        /// 当前执行过的帧数
         /// </summary>
         public int Frame;
 
@@ -33,8 +33,8 @@ namespace Aquaivy.Unity.Tasks
         public object Tag;
 
         /// <summary>
-        /// 执行函数
-        /// 注意，timer的时间为时间启动后的时间，而不是间隔时间
+        /// 执行函数，
+        /// 注意：timer的时间为时间启动后的时间，而不是间隔时间
         /// </summary>
         public Func<int, bool> Func { get; set; }
 
@@ -53,9 +53,6 @@ namespace Aquaivy.Unity.Tasks
         /// </summary>
         public int ErrorCount;
 
-        private bool waitCleaned = false;
-
-
         /// <summary>
         /// 释放这个任务
         /// </summary>
@@ -66,75 +63,57 @@ namespace Aquaivy.Unity.Tasks
 
         #region 静态方法
 
-        private static List<TaskLite> m_invokeActions = new List<TaskLite>();
-        private static List<TaskLite> m_willAddInvokeActions = new List<TaskLite>();
+        private static List<TaskLite> m_tasks = new List<TaskLite>(8);
+        private static int currentOperateTaskIndex = 0;
 
         /// <summary>
         /// 压入一个执行函数
-        /// 如果函数执行完成返回true
-        /// 如果返回false，则再下一帧还会继续调用
+        /// 如果返回true，则表示函数执行完成
+        /// 如果返回false，则下一帧还会继续执行
         /// </summary>
         /// <param name="action"></param>
         /// <returns></returns>
         public static TaskLite Invoke(Func<int, bool> action, int delayFrame = 0, object tag = null)
         {
-            var r = new TaskLite();
-            r.Func = action;
-            r.DelayFrame = delayFrame;
-            r.Tag = tag;
-            m_willAddInvokeActions.Add(r);
+            var r = new TaskLite
+            {
+                Func = action,
+                DelayFrame = delayFrame,
+                Tag = tag
+            };
+            m_tasks.Add(r);
             return r;
         }
 
         public static void Update(int elapseTime)
         {
-            if (m_willAddInvokeActions.Count > 0)
-            {
-                m_invokeActions.AddRange(m_willAddInvokeActions);
-                m_willAddInvokeActions.Clear();
-            }
+            if (m_tasks.Count <= 0)
+                return;
 
-            if (m_invokeActions.Count > 0)
+            for (currentOperateTaskIndex = 0; currentOperateTaskIndex < m_tasks.Count; currentOperateTaskIndex++)
             {
-                for (var i = 0; i < m_invokeActions.Count; i++)
+                var task = m_tasks[currentOperateTaskIndex];
+                task.Timer += elapseTime;
+                task.Frame++;
+
+                if (task.Frame >= task.DelayFrame)
                 {
-                    m_invokeActions[i].Timer += elapseTime;
-                    m_invokeActions[i].Frame++;
-                    if (m_invokeActions[i].Frame >= m_invokeActions[i].DelayFrame
-                        && !m_invokeActions[i].waitCleaned)
+                    try
                     {
-                        try
+                        task.IsDone = task.Func(task.Timer);
+
+                        if (task.IsDone)
                         {
-                            m_invokeActions[i].IsDone = m_invokeActions[i].Func(m_invokeActions[i].Timer);
-
-                            if (m_invokeActions[i].IsDone)
-                            {
-                                m_invokeActions.RemoveAt(i);
-                                i--;
-                            }
+                            Release(task);
                         }
-                        catch (Exception ex)
-                        {
-                            Logs.Warn("run tasklite.invoke fail. {0}", ex.Message);
-
-                            m_invokeActions[i].waitCleaned = true;
-                        }
-
                     }
-                }
+                    catch (Exception ex)
+                    {
+                        Logs.Warn("run tasklite.invoke fail. {0}", ex.Message);
 
-                CleanLoseEfficacyTasks();
-            }
-        }
+                        Release(task);
+                    }
 
-        private static void CleanLoseEfficacyTasks()
-        {
-            for (var i = 0; i < m_invokeActions.Count; i++)
-            {
-                if (m_invokeActions[i].waitCleaned)
-                {
-                    m_invokeActions.RemoveAt(i);
-                    i--;
                 }
             }
         }
@@ -142,18 +121,34 @@ namespace Aquaivy.Unity.Tasks
 
         private static void Release(TaskLite task)
         {
-            //Debug.Log($"TaskLite {task.Tag} releasing");
+            int index = m_tasks.IndexOf(task);
+            if (index < 0)
+                return;
 
-            task.waitCleaned = true;
-
-            m_willAddInvokeActions.Remove(task);
-            //m_invokeActions.Remove(task);
+            int current = currentOperateTaskIndex;
+            if (index <= current)
+            {
+                //移除当前索引之前的task
+                //移除当前索引的task
+                m_tasks.RemoveAt(index);
+                currentOperateTaskIndex--;
+            }
+            else
+            {
+                //移除当前索引的之后的task
+                m_tasks.RemoveAt(index);
+            }
         }
 
         public static void ReleaseAll()
         {
-            m_willAddInvokeActions.ForEach(o => o.Release());
-            m_invokeActions.ForEach(o => o.Release());
+            m_tasks.Clear();
+            currentOperateTaskIndex = 0;
+        }
+
+        public static string GetInfo()
+        {
+            return $"All TaskLites count: {m_tasks.Count}";
         }
 
         #endregion
