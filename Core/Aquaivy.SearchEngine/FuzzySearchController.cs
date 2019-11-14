2c1ef5185a69c0a2f11e226089516499b074c351
Lv Junxiao <lvjx1@lenovo.com>
2019-11-14 15:50:20 +0800

    .

--- Core/Aquaivy.SearchEngine/FuzzySearchController.cs
+++ Core/Aquaivy.SearchEngine/FuzzySearchController.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FuzzySearch
+namespace Aquaivy.SearchEngine
 {
     public static class FuzzySearchController
     {
