11f42e15716e414d8d100f9e6408ae2a74ed965e
Lv Junxiao <lvjx1@lenovo.com>
2020-05-06 15:57:37 +0800

    timeline

---
+++ UnityForAquaivy/Assets/UnitTest/09_Timeline/Test_09_Timeline.cs
@@ -0,0 +1,66 @@
+﻿using Aquaivy.Core.Logs;
+using Aquaivy.Unity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace NameSpace_Test_09_Timeline
+{
+    public class Test_09_Timeline : MonoBehaviour
+    {
+        public PlayableDirector Playable;
+
+        private void Start()
+        {
+            Playable.played += p => Log.Info("Playable.played");
+            Playable.stopped += p => Log.Info("Playable.stopped");
+            Playable.paused += p => Log.Info("Playable.paused");
+
+            ThrottleTask.Invoke(() =>
+            {
+                Log.Info(Playable.time.ToString());
+            }, 1000);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                PlayTimeline();
+            }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                StopTimeline();
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                PauseTimeline();
+            }
+
+            else if (Input.GetKeyDown(KeyCode.Z))
+            {
+                Playable.time = 0;
+            }
+        }
+
+        private void PlayTimeline()
+        {
+            Log.Info("Play");
+            Playable.Play();
+        }
+
+        private void StopTimeline()
+        {
+            Log.Info("Stop");
+            Playable.Stop();
+        }
+
+        private void PauseTimeline()
+        {
+            Log.Info("Pause");
+            Playable.Pause();
+        }
+    }
+}
