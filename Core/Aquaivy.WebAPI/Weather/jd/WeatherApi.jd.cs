89dc777a03c93a77405cfee8f4c9f92b1116bb90
lvjx <lvjx@yosim.club>
2020-07-16 14:19:46 +0800

    add jd weather

---
+++ Core/Aquaivy.WebAPI/Weather/jd/WeatherApi.jd.cs
@@ -0,0 +1,50 @@
+﻿using Aquaivy.Core.Webs;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aquaivy.WebAPI.Weather
+{
+    /// <summary>
+    /// 获取ip
+    /// </summary>
+    public partial class WeatherApi
+    {
+        public class jdResponseData
+        {
+
+        }
+
+        /// <summary>
+        /// 获取指定城市id的天气（为完成开发）
+        /// </summary>
+        /// <returns></returns>
+        public static jdResponseData GetWeatherByjdApi()
+        {
+            //网址url：https://wx.jdcloud.com/market/datas/26/10610
+            string url = "https://way.jd.com/he/freeweather";
+
+            jdResponseData data = null;
+
+            var response = HttpRequestUtils.Get(url);
+            if (response.Success)
+            {
+                var s = response.ResponseString;
+
+
+                data = JsonConvert.DeserializeObject<jdResponseData>(s);
+                Console.WriteLine(s);
+            }
+            else
+            {
+                data = new jdResponseData { };
+            }
+
+            return data;
+        }
+    }
+
+}
