30c2a262f0c3682daadb426e8064f77775c7a724
lvjx <lvjx@yosim.club>
2020-08-03 16:26:10 +0800

    .

--- Unity/Aquaivy.Unity.Editor/EditorTools.cs
+++ Unity/Aquaivy.Unity.Editor/EditorTools.cs
@@ -73,68 +73,68 @@ namespace Aquaivy.Unity.Editor
 
         #region Scene Loader
 
-        [MenuItem("Tools/Load Scene/Scene 0 %#1", false, 1004)]
+        [MenuItem("Tools/Load Scene/Scene 0 %#1", false, 1010)]
         public static void Load_Scene_0() => EditorSceneLoader.LoadScene(0);
 
-        [MenuItem("Tools/Load Scene/Scene 1 %#2", false, 1005)]
+        [MenuItem("Tools/Load Scene/Scene 1 %#2", false, 1010)]
         public static void Load_Scene_1() => EditorSceneLoader.LoadScene(1);
 
-        [MenuItem("Tools/Load Scene/Scene 2 %#3", false, 1006)]
+        [MenuItem("Tools/Load Scene/Scene 2 %#3", false, 1010)]
         public static void Load_Scene_2() => EditorSceneLoader.LoadScene(2);
 
-        [MenuItem("Tools/Load Scene/Scene 3 %#4", false, 1007)]
+        [MenuItem("Tools/Load Scene/Scene 3 %#4", false, 1010)]
         public static void Load_Scene_3() => EditorSceneLoader.LoadScene(3);
 
-        [MenuItem("Tools/Load Scene/Scene 4 %#5", false, 1008)]
+        [MenuItem("Tools/Load Scene/Scene 4 %#5", false, 1010)]
         public static void Load_Scene_4() => EditorSceneLoader.LoadScene(4);
 
-        [MenuItem("Tools/Load Scene/Scene 5 %#6", false, 1009)]
+        [MenuItem("Tools/Load Scene/Scene 5 %#6", false, 1010)]
         public static void Load_Scene_5() => EditorSceneLoader.LoadScene(5);
 
         [MenuItem("Tools/Load Scene/Scene 6 %#7", false, 1010)]
         public static void Load_Scene_6() => EditorSceneLoader.LoadScene(6);
 
-        [MenuItem("Tools/Load Scene/Scene 7 %#8", false, 1011)]
+        [MenuItem("Tools/Load Scene/Scene 7 %#8", false, 1010)]
         public static void Load_Scene_7() => EditorSceneLoader.LoadScene(7);
 
-        [MenuItem("Tools/Load Scene/Scene 8 %#9", false, 1012)]
+        [MenuItem("Tools/Load Scene/Scene 8 %#9", false, 1010)]
         public static void Load_Scene_8() => EditorSceneLoader.LoadScene(8);
 
-        [MenuItem("Tools/Load Scene/Scene 9 %#`", false, 1013)]
+        [MenuItem("Tools/Load Scene/Scene 9 %#`", false, 1010)]
         public static void Load_Scene_9() => EditorSceneLoader.LoadScene(9);
 
         #endregion
 
         #region GameObjectLocater
 
-        [MenuItem("Tools/Locate/Load Hierarchy 1 %1", false, 1021)]
+        [MenuItem("Tools/Locate/Load Hierarchy 1 %1", false, 1011)]
         public static void Locate_Hierarchy_1() => GameObjectLocater.Locate(0);
 
-        [MenuItem("Tools/Locate/Load Hierarchy 2 %2", false, 1022)]
+        [MenuItem("Tools/Locate/Load Hierarchy 2 %2", false, 1011)]
         public static void Locate_Hierarchy_2() => GameObjectLocater.Locate(1);
 
-        [MenuItem("Tools/Locate/Load Hierarchy 3 %3", false, 1023)]
+        [MenuItem("Tools/Locate/Load Hierarchy 3 %3", false, 1011)]
         public static void Locate_Hierarchy_3() => GameObjectLocater.Locate(2);
 
-        [MenuItem("Tools/Locate/Load Hierarchy 4 %4", false, 1024)]
+        [MenuItem("Tools/Locate/Load Hierarchy 4 %4", false, 1011)]
         public static void Locate_Hierarchy_4() => GameObjectLocater.Locate(3);
 
-        [MenuItem("Tools/Locate/Load Hierarchy 5 %5", false, 1025)]
+        [MenuItem("Tools/Locate/Load Hierarchy 5 %5", false, 1011)]
         public static void Locate_Hierarchy_5() => GameObjectLocater.Locate(4);
 
-        [MenuItem("Tools/Locate/Load Hierarchy 6 %6", false, 1026)]
+        [MenuItem("Tools/Locate/Load Hierarchy 6 %6", false, 1011)]
         public static void Locate_Hierarchy_6() => GameObjectLocater.Locate(5);
 
-        [MenuItem("Tools/Locate/Load Hierarchy 7 %7", false, 1027)]
+        [MenuItem("Tools/Locate/Load Hierarchy 7 %7", false, 1011)]
         public static void Locate_Hierarchy_7() => GameObjectLocater.Locate(6);
 
-        [MenuItem("Tools/Locate/Load Hierarchy 8 %8", false, 1028)]
+        [MenuItem("Tools/Locate/Load Hierarchy 8 %8", false, 1011)]
         public static void Locate_Hierarchy_8() => GameObjectLocater.Locate(7);
 
-        [MenuItem("Tools/Locate/Load Hierarchy 9 %9", false, 1029)]
+        [MenuItem("Tools/Locate/Load Hierarchy 9 %9", false, 1011)]
         public static void Locate_Hierarchy_9() => GameObjectLocater.Locate(8);
 
-        [MenuItem("Tools/Locate/Load Hierarchy 10 %10", false, 1030)]
+        [MenuItem("Tools/Locate/Load Hierarchy 10 %10", false, 1011)]
         public static void Locate_Hierarchy_10() => GameObjectLocater.Locate(8);
 
 
@@ -142,34 +142,34 @@ namespace Aquaivy.Unity.Editor
 
 
 
-        [MenuItem("Tools/Locate/Set Locate 1", false, 1031)]
+        [MenuItem("Tools/Locate/Set Locate 1", false, 1012)]
         public static void Set_Locate_1() => GameObjectLocater.SetLocateConfig(0);
 
-        [MenuItem("Tools/Locate/Set Locate 2", false, 1032)]
+        [MenuItem("Tools/Locate/Set Locate 2", false, 1012)]
         public static void Set_Locate_2() => GameObjectLocater.SetLocateConfig(1);
 
-        [MenuItem("Tools/Locate/Set Locate 3", false, 1033)]
+        [MenuItem("Tools/Locate/Set Locate 3", false, 1012)]
         public static void Set_Locate_3() => GameObjectLocater.SetLocateConfig(2);
 
-        [MenuItem("Tools/Locate/Set Locate 4", false, 1034)]
+        [MenuItem("Tools/Locate/Set Locate 4", false, 1012)]
         public static void Set_Locate_4() => GameObjectLocater.SetLocateConfig(3);
 
-        [MenuItem("Tools/Locate/Set Locate 5", false, 1035)]
+        [MenuItem("Tools/Locate/Set Locate 5", false, 1012)]
         public static void Set_Locate_5() => GameObjectLocater.SetLocateConfig(4);
 
-        [MenuItem("Tools/Locate/Set Locate 6", false, 1036)]
+        [MenuItem("Tools/Locate/Set Locate 6", false, 1012)]
         public static void Set_Locate_6() => GameObjectLocater.SetLocateConfig(5);
 
-        [MenuItem("Tools/Locate/Set Locate 7", false, 1037)]
+        [MenuItem("Tools/Locate/Set Locate 7", false, 1012)]
         public static void Set_Locate_7() => GameObjectLocater.SetLocateConfig(6);
 
-        [MenuItem("Tools/Locate/Set Locate 8", false, 1038)]
+        [MenuItem("Tools/Locate/Set Locate 8", false, 1012)]
         public static void Set_Locate_8() => GameObjectLocater.SetLocateConfig(7);
 
-        [MenuItem("Tools/Locate/Set Locate 9", false, 1039)]
+        [MenuItem("Tools/Locate/Set Locate 9", false, 1012)]
         public static void Set_Locate_9() => GameObjectLocater.SetLocateConfig(8);
 
-        [MenuItem("Tools/Locate/Set Locate 10", false, 1040)]
+        [MenuItem("Tools/Locate/Set Locate 10", false, 1012)]
         public static void Set_Locate_10() => GameObjectLocater.SetLocateConfig(9);
 
         #endregion
